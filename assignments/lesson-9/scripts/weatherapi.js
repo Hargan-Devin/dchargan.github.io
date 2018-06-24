4915246031e376a6feb3424e2f42544afedf2e1e
Devin <dchargan@gmail.com>
2018-06-23 21:11:10 -0700

    Lesson 9

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,11 +1,13 @@
 var weatherRequest = new XMLHttpRequest();
-var apiURL =
-        'http://samples.openweathermap.org/data/2.5/forecast?id=4156210&appid=5cd1df0f20e7b178b00435263d755f1f';
+var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=4156210&appid=5cd1df0f20e7b178b00435263d755f1f&units=imperial';
 weatherRequest.open("GET",apiURL,true);
-weatherRequest.responseType = 'json';
+weatherRequest.responseType = 'text';
 weatherRequest.send();
 weatherRequest.onload = function() {
     var weatherData = JSON.parse(weatherRequest.responseText);
     console.log(weatherData);
+    
+    var temperature = weatherData.list[0].main.temp;
+    document.getElementById('currentTemp').innerHTML = temperature;
 }
 
